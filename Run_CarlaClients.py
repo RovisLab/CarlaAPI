fb794e35a44646eb70f002fe1a7fded9f0672b2c
Sorin Grigorescu <Sorin.Grigorescu@elektrobit.com>
2022-11-13 07:04:45 +0200

    added 2 cars

--- Run_CarlaClients.py
+++ Run_CarlaClients.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
         print(' # Carla simulator could not be reached. Exiting..')
         exit()
 
-    client = CarlaClient(name='Client',  # Name of the client
+    car_01 = CarlaClient(name='Car One',  # Name of the client
                          view_cam=True,  # View car from eagle view
                          position=carla.Location(-77.3, 74.4, 1),
                          orientation=carla.Rotation(0, -90, 0),
@@ -66,10 +66,10 @@ if __name__ == "__main__":
                          ip_rovis='127.0.0.1',
                          port_rovis_actuator=2003,            # 2003
                          port_rovis_camera_front=2004,        # 2004
-                         port_rovis_camera_back=None,         # 2005
-                         port_rovis_semseg_camera=None,       # 2006
+                         port_rovis_camera_back=2005,         # 2005
+                         port_rovis_semseg_camera=2006,       # 2006
                          port_rovis_state_measurement=2010,   # 2010
-                         port_rovis_imu=None,                 # 2011
+                         port_rovis_imu=2011,                 # 2011
                          port_rovis_depth=None,               # 2020
                          port_rovis_lidar=None,               # 2021
                          port_rovis_radar=None,               # 2022
@@ -79,36 +79,64 @@ if __name__ == "__main__":
                          view_height=480,
                          view_fov=90)
 
-    # client_2 = CarlaClient(name='Client Two',
-    #                        view_cam=True,
-    #                        position=carla.Location(-73, 103, 1),
-    #                        orientation=carla.Rotation(0, -89, 0),
-    #                        random_spawn=False,
-    #                        vehicle_type='vehicle.tesla.model3',
-    #                        control='auto',
-    #
-    #                        ip_carla='127.0.0.1',
-    #                        port_carla=2000,
-    #
-    #                        ip_rovis='127.0.0.1',
-    #                        port_rovis_actuator=None,            # 2103
-    #                        port_rovis_camera_front=2104,        # 2104
-    #                        port_rovis_camera_back=2105,         # 2105
-    #                        port_rovis_semseg_camera=None,       # 2106
-    #                        port_rovis_state_measurement=2110,   # 2110
-    #                        port_rovis_imu=None,                 # 2111
-    #                        port_rovis_depth=None,               # 2120
-    #                        port_rovis_lidar=None,               # 2121
-    #                        port_rovis_radar=None,               # 2122
-    #
-    #                        target_fps=30,
-    #                        view_width=640,
-    #                        view_height=480,
-    #                        view_fov=90)
+    car_02 = CarlaClient(name='Client Two',
+                           view_cam=True,
+                           position=carla.Location(-73, 103, 1),
+                           orientation=carla.Rotation(0, -89, 0),
+                           random_spawn=False,
+                           vehicle_type='vehicle.tesla.model3',
+                           control='auto',
+
+                           ip_carla='127.0.0.1',
+                           port_carla=2000,
+
+                           ip_rovis='127.0.0.1',
+                           port_rovis_actuator=None,            # 2103
+                           port_rovis_camera_front=2104,        # 2104
+                           port_rovis_camera_back=2105,         # 2105
+                           port_rovis_semseg_camera=None,       # 2106
+                           port_rovis_state_measurement=2110,   # 2110
+                           port_rovis_imu=None,                 # 2111
+                           port_rovis_depth=None,               # 2120
+                           port_rovis_lidar=None,               # 2121
+                           port_rovis_radar=None,               # 2122
+
+                           target_fps=30,
+                           view_width=640,
+                           view_height=480,
+                           view_fov=90)
+
+    car_02 = CarlaClient(name='Car One',  # Name of the client
+                         view_cam=True,  # View car from eagle view
+                         position=carla.Location(-77.3, 64.4, 1),
+                         orientation=carla.Rotation(0, -90, 0),
+                         random_spawn=False,  # Get a random spawn point or spawn at the position above
+                         vehicle_type='vehicle.tesla.model3',  # Vehicle type
+                         control='rovis',  # auto / rovis
+                         town_name='Town03',  # Change the map if needed
+
+                         ip_carla='127.0.0.1',  # Loopback ip for Carla
+                         port_carla=2000,  # Carla port
+
+                         ip_rovis='127.0.0.1',
+                         port_rovis_actuator=2103,  # 2103
+                         port_rovis_camera_front=2104,  # 2104
+                         port_rovis_camera_back=2105,  # 2105
+                         port_rovis_semseg_camera=2106,  # 2106
+                         port_rovis_state_measurement=2110,  # 2110
+                         port_rovis_imu=2111,  # 2111
+                         port_rovis_depth=None,  # 2120
+                         port_rovis_lidar=None,  # 2121
+                         port_rovis_radar=None,  # 2122
+
+                         target_fps=30,
+                         view_width=640,
+                         view_height=480,
+                         view_fov=90)
 
     try:
-        threading.Thread(target=client.game_loop).start()
-        # threading.Thread(target=client_2.game_loop).start()
+        threading.Thread(target=car_01.game_loop).start()
+        threading.Thread(target=car_02.game_loop).start()
     except RuntimeError as e:
         print("Runtime error encountered.")
         print(e)
